ef2e3fc5de6b31c53319cf133bc34d92653fa67c
neerakara <nkarani@csail.mit.edu>
2023-01-09 18:27:27 -0500

    adapted permutation test to account for variance across runs

--- significance_tests.py
+++ significance_tests.py
@@ -21,7 +21,7 @@ import csv
 # ==========================================
 def get_results(args):
     
-    num_runs = 2
+    num_runs = 3
     results = np.zeros((40, num_runs))
     
     for r in range(num_runs):
@@ -52,24 +52,34 @@ def get_results(args):
     return results
 
 # ==========================================
+# Carries out a paired permutation test
+# with differences between the two methods for each subject
+# scaled down by the geometric mean of the standard deviations of each method across training runs.
 # ==========================================
 def compute_significance(r1, r2, s1, s2, cv):
     mu1 = np.mean(r1, axis=-1)
     mu2 = np.mean(r2, axis=-1)
-    # std1 = np.std(r1, axis=-1)
-    # std2 = np.std(r2, axis=-1)
-    # diff = (mu2 - mu1) / np.sqrt(std1 * std2 + 1e-15)
+    std1 = np.std(r1, axis=-1)
+    std2 = np.std(r2, axis=-1)
+    den = np.sqrt(std1 * std2)
+    # hack to deal with zero std devs
+    den[den == 0.0] = 100.0
+    den_tmp = np.min(den)
+    den[den == 100.0] = den_tmp
+    # differences with high std devs across runs will have small weights
+    diff = (mu2 - mu1) / den
+    # diff = mu2 - mu1
     # logging.info(np.mean(diff))
     # logging.info(np.std(diff))
 
     np.random.seed(1234)
     
-    orig_diff = np.mean(mu2 - mu1)
+    orig_diff = np.mean(diff)
     num_permutes = 10**4
     permute_diffs = 1234 * np.ones((num_permutes))
     for n in range(num_permutes):
         tmp = np.random.choice([-1,1], size=mu1.shape[0], replace=True)
-        permute_diffs[n] = np.mean(tmp * (mu2 - mu1))
+        permute_diffs[n] = np.mean(tmp * diff)
     
     if orig_diff > 0.0:
         num = len(np.where(permute_diffs > orig_diff)[0]) + 1
